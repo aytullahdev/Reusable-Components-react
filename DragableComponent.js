0c5cb79ef0903cf7cddb6326b24d3dcd4ac010b6
aytullahdev <58880826+aytullahdev@users.noreply.github.com>
2023-09-20 01:21:04 +0600

    Create DragableComponent.js

---
+++ DragableComponent.js
@@ -0,0 +1,34 @@
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+const [sectionPosition, setSectionPositions] = useState(Object.keys(templateData).map((id) => ({ "id": id + "drage", "sectionName": id })))
+
+    const handleOnDragEnd = (result) => {
+
+        const items = Array.from(sectionPosition);
+        const [reorderedItem] = items.splice(result.source.index, 1);
+        items.splice(result.destination.index, 0, reorderedItem)
+        setSectionPositions(items);
+    };
+<DragDropContext onDragEnd={handleOnDragEnd} >
+                <Droppable droppableId="sections">
+                    {(provided) => (
+                        <div className='col-span-5 ' {...provided.droppableProps} ref={provided.innerRef}>
+                            {sectionPosition.map(({ sectionName, id }, indx) => {
+                                return (
+                                    <Draggable key={id} draggableId={id} index={indx}>
+                                        {(provided) => (
+                                            <div
+                                                ref={provided.innerRef}
+                                                {...provided.draggableProps}
+                                                {...provided.dragHandleProps}
+                                            >
+                                                <EditSection templateData={templateData} sectionName={sectionName} setTemplateData={setTemplateData} />
+                                            </div>
+                                        )}
+                                    </Draggable>
+                                );
+                            })}
+                            {provided.placeholder}
+                        </div>
+                    )}
+                </Droppable>
+            </DragDropContext>
