122341d64c618b7bac9116c2ccc28b502bbe77af
aytullahdev <58880826+aytullahdev@users.noreply.github.com>
2023-09-20 01:28:26 +0600

    Update DragableComponent.js

--- DragableComponent.js
+++ DragableComponent.js
@@ -1,13 +1,15 @@
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 const [sectionPosition, setSectionPositions] = useState(Object.keys(templateData).map((id) => ({ "id": id + "drage", "sectionName": id })))
 
-    const handleOnDragEnd = (result) => {
-
+   const handleOnDragEnd = (result) => {
+        if ((!result.destination.index || !result.source.index) && (result.destination.index !== 0 && result.source.index !== 0)) return;
         const items = Array.from(sectionPosition);
         const [reorderedItem] = items.splice(result.source.index, 1);
         items.splice(result.destination.index, 0, reorderedItem)
         setSectionPositions(items);
     };
+
+
 <DragDropContext onDragEnd={handleOnDragEnd} >
                 <Droppable droppableId="sections">
                     {(provided) => (
